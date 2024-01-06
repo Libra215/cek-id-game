d0d537a97e94bc493eb1adb7cd104b192458926d
Hamdan Mubarok <89371232+Libra215@users.noreply.github.com>
2024-01-06 22:24:55 +0700

    Update-1 app.js

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
 
 const baseURL = axios.create({
-	baseURL: "https://api.kitadigital.my.id/api"
+	baseURL: "https://api.kitadigital.my.id"
 })
 
 app.get('/', async (req, res) => {
 	const url = await axios.get("https://api.kitadigital.my.id/api", {
 		headers: {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json; charset=utf-8'
 		}
 	}).then(response => {
+    res.status(200)
 		res.json({
 			name: response.data.name,
 			author: 'Hmdan_dev',
 			instagram: 'https://www.instagram.com/hmd_dev',
+      api_bank: 'https://api-bank.hmdan214.repl.co/api/bank-docs',
 			data: response.data.data
 		});
 		// console.log(response.data.data);
@@ -33,18 +37,21 @@ app.get('/api/:type/:data', async (req, res) => {
 			zone:  req.query.zone
 		},
 		headers: {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json; charset=utf-8'
 		}
 	}).then(response => {
+    console.log(response.data);
+		console.log(response.status);
 		res.json(response.data);
-		console.log(response.data);
+    res.status(response.status);
 	}).catch(error => {
 
 		if (error.response) {
 			console.error('Status:', error.response.status);
 			console.error('Data:', error.response.data);
-			res.json(error.response.data);
-			res.status(404);
+
+res.status(error.response.status);   res.json(error.response.data);
+      
 			res.end();
 				// process.exit()
 			} else if (error.request) {
@@ -64,18 +71,22 @@ app.get('/api/:type/:data/:zone', async (req, res) => {
 			zone:  req.query.zone
 		},
 		headers: {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json; charset=utf-8'
 		}
 	}).then(response => {
+    console.log(response.data);
+		console.log(response.status);
 		res.json(response.data);
-		console.log(response.data);
+    res.status(response.status);
 	}).catch(error => {
 
 		if (error.response) {
 			console.error('Status:', error.response.status);
 			console.error('Data:', error.response.data);
-			res.json(error.response.data);
-			res.status(404);
+			
+res.status(error.response.status);
+      res.json(error.response.data);
+      
 			res.end();
 				// process.exit()
 			} else if (error.request) {
@@ -87,5 +98,8 @@ app.get('/api/:type/:data/:zone', async (req, res) => {
 
 })
 
+app.get('/status', (req, res) => {
+  res.status(404)
+})
 
 app.listen(3000)
